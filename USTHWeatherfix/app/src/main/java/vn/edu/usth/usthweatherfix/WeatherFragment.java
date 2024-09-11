c992fe3d7c19df9dd6639f4f7d18a29cc1d7fcce
nmdinusth <ducnmba12-050@st.usth.edu.vn>
2024-09-11 20:49:00 +0700

    Practice 6

---
+++ USTHWeatherfix/app/src/main/java/vn/edu/usth/usthweatherfix/WeatherFragment.java
@@ -0,0 +1,25 @@
+package vn.edu.usth.usthweatherfix;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class WeatherFragment extends Fragment {
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
