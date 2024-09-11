4f02e639f625d2e16ed9bac0969796673795ce68
nmdinusth <ducnmba12-050@st.usth.edu.vn>
2024-09-11 15:57:12 +0700

    Practical 2

--- USTHWeatherfix/app/src/main/java/vn/edu/usth/usthweatherfix/WeatherActivity.java
+++ USTHWeatherfix/app/src/main/java/vn/edu/usth/usthweatherfix/WeatherActivity.java
@@ -1,7 +1,7 @@
 package vn.edu.usth.usthweatherfix;
 
 import android.os.Bundle;
-
+import android.util.Log;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class WeatherActivity extends AppCompatActivity {
-
+    private static final String WA = "WeatherActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +20,36 @@ public class WeatherActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.d(WA,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(WA, "Oncreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(WA, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(WA,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(WA,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(WA, "onDestroy");
     }
 }
